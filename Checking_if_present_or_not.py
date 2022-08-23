5f8945d95619828086362daa802a7f308e826002
AbhiH11 <abhihari119@gmail.com>
2022-08-23 15:38:42 +0530

    Checking if present or not in a txt

---
+++ Checking_if_present_or_not.py
@@ -0,0 +1,8 @@
+txt = 'I am going home after the meeting'
+
+# if 'going' in txt:
+#     print('yes','going is present in txt')
+
+if 'abhinay' not in txt:
+    print('No','abhinay is not present in txt')
+
