f561d1172b6178e4f466e8bff140f92a8f06bd55
AbhiH11 <abhihari119@gmail.com>
2022-11-11 16:38:22 +0530

    Added new code

--- Crash_course.py
+++ Crash_course.py
@@ -173,3 +173,91 @@ car = 'audi'
 
 # if car != 'zoom':
 #     print('Try again')
+
+####--IF AND ELSE AND ELIF--####
+
+# age = int(input('Please enter your age: \n'))
+# if age < 4:
+#     price = 20
+#     # print("Adission fee is Rs 20")
+# elif age < 18:
+#     price = 25
+#     # print("admission fee is Rs 25")
+# elif age >=60:
+#     price = 50
+#     # print("admission fee is Rs 50")
+# print(f'Your admission cost is {price}')
+
+# alien_color = 'green'
+#
+# if alien_color == "blue":
+#     print("Player just earned 5 points")
+# else:
+#     print("Player just earned 10 points")
+
+# # alien_color = 'green'
+# alien_color = 'yellow'
+# alien_color = 'red'
+#
+# if alien_color == "green":
+#     print("Player just earned 5 points")
+# elif alien_color == 'yellow':
+#     print("player just earned 10 points")
+# else:
+#     print("Player just earned 20 points")
+
+
+# age = int(input('Please enter your age: \n'))
+# if age < 2:
+#     print("You are a baby.!")
+# elif age >= 2 and age < 4:
+#     print("You are a toddler.!")
+# elif age >= 4 and age < 13:
+#     print("you are a kid.!")
+# elif age >= 13 and age < 20:
+#     print("you are a teenager.!")
+# elif age >= 20 and age < 65:
+#     print("you are an adult.!")
+# else:
+#     print("you are an elder.!")
+
+# fav_fruit = ['apple','mango','grapes','orange','pineapple']
+# if 'apple' in fav_fruit:
+#     print('You really like apple')
+# if 'mango' in fav_fruit:
+#     print("\nyou really like mango")
+# if 'grapes' in fav_fruit:
+#     print("\nyou really like grapes")
+# if 'orange' in fav_fruit:
+#     print("\nyou really like orange")
+# if 'pineapple' in fav_fruit:
+#     print("\nyou really like pineapple")
+
+
+# available_toppings = ['mushroom','olives','green peppers','pepparoni','pineapple','extra cheese']
+# requested_toppings = ['olives','green peppers','mushroom','french fries']
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print(f'adding {requested_topping.title()}')
+#     else:
+#         print(f"sorry we dont have {requested_topping.title()}")
+# print('\nfinished making your pizza')
+
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print(f'Adding {requested_topping} ')
+#         print('Finished making your pizza')
+# print("\nDo you really need a plain pizza")
+
+# names = ['admin','abhi_h','anu_h','sonu_k']
+names = []
+if names:
+    for name in names:
+        if name == 'admin':
+            print("Hello Admin, would you like to see a status report?")
+        else:
+            print(f"Hello {name.title()}, thank you for logging in again.!")
+else:
+    # print(f"Hello {name}, thank you for logging in again.!")
+    print("We need to find some users.!")
+print('\nThank you have a Great day..!')
