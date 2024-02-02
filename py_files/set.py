a894c9d6ea5ba20950d79a4dcffe2e58dac3fe20
AbhiH11 <abhihari119@gmail.com>
2024-02-02 14:34:22 +0530

    practising setPractising sets

---
+++ py_files/set.py
@@ -0,0 +1,25 @@
+# x = set()
+# print(type(x))
+
+# x = {"abhi", 5, 9 ,5 ,2, "abhi"}
+# # print(x)
+
+# for i in x:
+#     print(i)
+
+s1 = {2,3,4,5,6,1,7,8}
+s2 = {5,6,7,8}
+# union = (s1.union(s2)) # addes both the sets
+# s1.update(s2) #updates s1
+# difference = s1.difference(s2) #Removes the common element
+# print(s1.issuperset(s2)) #returns True if the elements are already in s1
+# print(s2.issubset(s1)) #returns True if the elements are already present in s1
+# s1.add(10) # add method isused to add the single element.
+# print(s1)
+# s1.remove(11) # throws an error if the element not in the set.
+# s1.discard(11) #doesnt throw an error if the element is not in the set
+# print(s1)
+# print(union)
+# print(s1)
+# print(difference)
+# print(s1.isdisjoint(s2)) #no element should be in common
