609a73a5a440aab93992dcfc85365185b52fe105
AbhiH11 <abhihari119@gmail.com>
2024-02-02 16:23:13 +0530

    Practising dict concept

--- dict2024.py
+++ dict2024.py
@@ -3,4 +3,26 @@ dic = { "Abhi": "Human",
        "Activa" : "object"
 }
 
-print(dic["Activa"])
+# print(dic["Activa"])
+
+# for key in dic:
+#        print(f"The value of key '{key}' is : {dic[key]}. ")
+
+# for key, value in dic.items():
+#     print(f"The value of key '{key}' is : {value}. ")
+
+'''Dictionery Methods Below'''
+
+dic1 = {13: 45, 34: 56}
+dic2 = {45:78, 123:78}
+# dic1.update(dic2)
+# print(dic1)
+
+# pop method:
+# dic1.pop(13)
+# print(dic1)
+
+# popitem():pops the last item from the dictionary
+
+# dic1.popitem()
+# print(dic1)
